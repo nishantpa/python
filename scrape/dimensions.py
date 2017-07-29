df785704526f88ffac4e35a8d1f0368bb72b0be4
Nishant Patel <nishantpatel@Nishants-MBP.home>
2017-07-29 14:55:25 -0400

    last update

--- scrape/dimensions.py
+++ scrape/dimensions.py
@@ -10,7 +10,6 @@ for args in sys.argv:
 	    soup = BeautifulSoup(fp, "html.parser")
 
 	for vehicleDetails in soup.find_all(class_="unit"):
-	    # print vehicleDetails.text.encode('utf-8')
 	    print vehicleDetails.text.encode('utf-8').split('L ')[0],
 	    print ",",  
 	    print vehicleDetails.text.encode('utf-8').replace(" ", "").split('H:')[1][0:14].replace("x", ",")
