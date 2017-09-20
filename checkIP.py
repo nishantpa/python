0a8ea5f41dc004701b8d177461330a80657aa880
Nishant Patel <thenishantpatel@gmail.com>
2017-09-20 13:37:29 -0400

    HackerRank in py

---
+++ checkIP.py
@@ -0,0 +1,45 @@
+# << Swami Shreeji >>
+# @nishantpatel
+# Complete the function below.
+
+import sys
+import os
+import string
+
+def checkIP(ip):
+    ans = []
+    countDot = 0
+    countCol = 0
+    for ele in ip:
+        countDot = ele.count(".") 
+        countCol = ele.count(":")
+            
+        if countDot < 3 and countCol < 7:
+            ans.append("Neither")
+            
+        elif countDot == 3: 
+            testIpv4 = ele.split(".")
+            
+            for char in testIpv4:
+                if char.isalpha():
+                    ans.append("Neither")
+                else:
+                    testIpv4 = map(int, testIpv4)
+                    for ele in testIpv4:
+                        if ele < 0 or ele > 255:
+                            ans.append("Neither")
+                        else:
+                            ans.append("IPv4")
+                            break
+                    break
+                    
+        elif countCol == 7:
+            testIpv6 = ele.split(":")
+            for ele in testIpv6:
+                if all(char in string.hexdigits for char in ele):
+                    ans.append("IPv6")
+                else:
+                    ans.append("Neither")
+                break
+           
+    return ans
---
+++ infoMasking.py
@@ -0,0 +1,45 @@
+# << Swami Shreeji >>
+# @nishantpatel
+# Mask email and phone number according to the rules
+
+# E:jackAndJill@gmail.com  					    # E:j*****l@gmail.com
+# P:(333)456-7890 				OUTPUT:			# P:***-***-7890
+# P:+1(333) 456-7890 							# P:+*-***-***-7890
+
+import sys
+
+# Process email
+
+while True:
+	testInput = raw_input()
+	
+	if testInput == "":
+		break
+
+	# Process email
+	elif testInput.startswith("E:"):
+		userName = testInput[2:testInput.find("@")]
+		domainName = testInput[testInput.find("@"):]
+		stars = "*" * 5
+		
+		userMasked = userName[0] + stars + userName[-1:] + domainName
+		print "E:" + userMasked
+
+	# Process phone number
+	elif testInput.startswith("P:"):
+		start = testInput[2:-4]
+		mask = "***-***-"
+		lastFour = testInput[-4:]
+
+		start = start.translate(None, "()- " )
+
+		if len(start) == 6:
+			ans = mask
+		elif len(start) == 8:
+			ans = "+*-" + mask
+		elif len(start) == 9:
+			ans = "+**-" + mask
+		elif len(start) == 10:
+			ans = "+***-" + mask
+
+		print "P:" + ans + lastFour
