2eea76d6d9f824b7eb698bf2b5de48029b55abed
nishantpa <thenishantpatel@gmail.com>
2017-10-06 10:28:26 -0400

    crawl attempt

---
+++ crawl/crawlScrape.py
@@ -0,0 +1,8 @@
+# << Swami Shreeji >>
+# @nishantpatel ; 6 Oct 2017
+	
+	# Jenkins plugin issue: When attempting to update Jenkins plugins, 
+	# hit an error that I suspect is due to dependency mismatches
+
+import sys
+print "Hello world"
