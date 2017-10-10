cd835af0521e608a9147d51840fb32aebdb0f7f9
nishantpa <thenishantpatel@gmail.com>
2017-10-10 13:56:40 -0400

    checkPlugs versions left

--- crawl/jenkinsWork/getURL.sh
+++ crawl/jenkinsWork/getURL.sh
@@ -3,5 +3,6 @@
 # << Swami Shreeji >>
 # @nishantpatel ; 10 Oct 2017
 # Create properly formed URL from jenkins plugin IDs
+# Command to execute on json output: sed 's/^.\{15\}//' dependents.json | cut -f 1 -f 3 -d ","
 
 cut -f 1 -d ":" jenkPlug.fail | sed 's/^/https:\/\/plugins.jenkins.io\//' > URLsList.txt
