8466f7fcf3c9f899bd5628a96743e9cecd8f0474
Nishant Patel <nishantpatel@Nishants-MBP.home>
2017-10-17 06:44:07 -0400

    updated challenge

--- hackerRank/twoChars.py
+++ hackerRank/twoChars.py
@@ -4,13 +4,29 @@
 
 import sys
 import collections
+from itertools import combinations
+import re
+import string
 
-# Take from stdin
 testStrLen = raw_input()
 testStr = raw_input()
 
 testStrInfo = collections.Counter(testStr)
 uniqueLetters = list(testStrInfo)
 
+# TESTING
 print uniqueLetters
-print uniqueLetters[1]
+
+# Get all pair combos
+storeCombos = list()
+for pair in combinations(uniqueLetters, 2):
+	 pairedUp = pair[0] + pair[1]
+	 storeCombos.append(pairedUp)
+
+i = 0
+while i != len(storeCombos):
+	for char in testStr:
+		if char != storeCombos[i]:
+			
+
+	#potentialAns = testStr.strip(everything except pair[0] and pair[1])
