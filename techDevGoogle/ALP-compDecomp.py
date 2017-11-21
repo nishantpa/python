ca8873acc8402fe7354a25623c1488b40220fd16
Nishant Patel <nishantpatel@Nishants-MBP.home>
2017-11-21 16:22:40 -0500

    updated to support double digits

--- techDevGoogle/ALP-compDecomp.py
+++ techDevGoogle/ALP-compDecomp.py
@@ -11,24 +11,27 @@ import sys
 import string
 
 # Test cases | Interpretted as a tuple
-case1 = "3[abc]4[ab]c"
+case1 = "10[abc]4[ab]c"
 case2 = "2[3[a]b]"
 
 def decompress(args):
 	# General Case
 	currString = ""
+	currInt = ""
 	repeater = 0
+	
 	for char in args:
 		if char.isdigit():
-			repeater = int(char)
-		if char.isalpha():
+			currInt = currInt + char
+		elif char.isalpha():
 			currString = currString + char
 		elif char == "]":
+			repeater = int (currInt)
 			for i in range(0, repeater):
 				sys.stdout.write(currString)
-			currString = "" 
-		sys.stdout.write(currString)
+			currString = ""
+			currInt = "" 
+	sys.stdout.write(currString)
 
-
-decompress(case2)
+decompress(case1)
 print ""
